ff7a680d6f1b7d5631c0f496bae373064e4be7c9
Adam Lynch <adamclynch03@gmail.com>
2024-09-28 21:27:19 -0400

    build: package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="nfldpw",
+    version="0.1.0",
+    description="Wrapper for nfl_data_py",
+    url="https://github.com/adam42739/nfldpw",
+    author="Adam Lynch",
+    author_email="aclynch@umich.edu",
+    license="MIT License",
+    packages=["nfldpw"],
+    install_requires=[
+        "nfl_data_py>=0.3.3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licesnse :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
