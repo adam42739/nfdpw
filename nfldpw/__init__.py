562f20dcd91b4be2a83553c6d15e9cb65b8e1e70
Adam Lynch <adamclynch03@gmail.com>
2024-09-29 14:12:17 -0400

    docs: package docstring

---
+++ nfldpw/__init__.py
@@ -0,0 +1,17 @@
+"""
+======
+nfldpw
+======
+
+A wrapper for [nfl_data_py](https://github.com/nflverse/nfl_data_py).
+
+Available subpackages
+---------------------
+
+Play-by-playdata:
+
+```python
+>>> import nfldpw.pbp as pbp
+```
+
+"""
