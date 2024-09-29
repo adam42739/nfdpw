b54da273e3f4433359ae65a57edd8d46a2e1565d
Adam Lynch <adamclynch03@gmail.com>
2024-09-29 14:11:44 -0400

    docs: subpackage docstring

--- nfldpw/pbp/__init__.py
+++ nfldpw/pbp/__init__.py
@@ -1,2 +1,20 @@
+"""
+=================
+Play-by-play data
+=================
+
+Use `get()` for general NFL play-by-play data.
+
+Available submodules
+--------------------
+
+cols:
+
+```python
+>>> import pbp.cols as cols
+```
+
+"""
+
 from .pbp import get
 from . import cols
