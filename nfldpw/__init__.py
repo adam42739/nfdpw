837ea0bd5a793fc419b5007ade725da247ca9685
Adam Lynch <adamclynch03@gmail.com>
2024-09-29 14:38:31 -0400

    docs: package docstring: schedules subpackage

--- nfldpw/__init__.py
+++ nfldpw/__init__.py
@@ -8,10 +8,16 @@ A wrapper for [nfl_data_py](https://github.com/nflverse/nfl_data_py).
 Available subpackages
 ---------------------
 
-Play-by-playdata:
+Play-by-play data:
 
 ```python
 >>> import nfldpw.pbp as pbp
+```
+
+Schedules data:
+
+```python
+>>> import nfldpw.schedules as schedules
 ```
 
 """
